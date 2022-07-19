87e092aac17265d71eb8ccfb0be6053b3dcdba1b
Marx Teixeira <95075808+marxteixeira@users.noreply.github.com>
2022-07-18 23:01:12 -0300

    Editor HTML: projeto completo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EditorHtml // Note: actual namespace depends on the project name.
+{
+    //internal class Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+        }
+    }
+}
